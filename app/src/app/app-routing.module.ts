a77caeb2b33240c16df3801023db214ed2b02303
Joan Toro Ortiz <joantoro@me.com>
2024-08-20 20:49:06 -0400

    proyecto base inicial

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -3,12 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: '',
     pathMatch: 'full'
   },
 ];
