381e9a83865f4ef9890403f01a5cd266896a0140
Joan Toro Ortiz <joantoro@me.com>
2024-09-24 21:27:43 -0300

    add config firebase

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCIqa1QkghaXCzTjRSsyJ485rr605GByUQ",
+    authDomain: "findit-2024.firebaseapp.com",
+    projectId: "findit-2024",
+    storageBucket: "findit-2024.appspot.com",
+    messagingSenderId: "63412124090",
+    appId: "1:63412124090:web:4f5f0de1bec10f0961c583"
+  },
 };
--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCIqa1QkghaXCzTjRSsyJ485rr605GByUQ",
+    authDomain: "findit-2024.firebaseapp.com",
+    projectId: "findit-2024",
+    storageBucket: "findit-2024.appspot.com",
+    messagingSenderId: "63412124090",
+    appId: "1:63412124090:web:4f5f0de1bec10f0961c583"
+  },
 };
 
 /*
